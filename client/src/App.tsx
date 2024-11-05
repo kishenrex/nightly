9c6c431e2f80eb5c014b8704ef965bb2a360ee9b
Clarence Bermudez <csbermudez04@gmail.com>
2024-11-04 19:19:43 -0800

    Created Timer Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
 import TimerPage from './pages/TimerPage';
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/landing/" element={<LandingPage />} />
          <Route path="/timer/" element={<TimerPage />} />
       </Routes>
     </div>
