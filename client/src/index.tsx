9df5506e18f6923bc3ad61fe37f8eb6b00d085b0
kishenrex <kishenthevan@gmail.com>
2024-11-09 21:03:34 -0800

    converted login to sign in with google

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,17 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <GoogleOAuthProvider clientId='146123879009-1qmqg1lcujqlv67m519dnsggf2v3u595.apps.googleusercontent.com'>
   <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </BrowserRouter>
-  
+  </GoogleOAuthProvider>  
 );
 
 // If you want to start measuring performance in your app, pass a function
