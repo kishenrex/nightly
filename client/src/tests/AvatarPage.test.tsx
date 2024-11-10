0c8b34decd7c208ec0b32d659113e63162a75a7c
Clarence Bermudez <csbermudez04@gmail.com>
2024-11-09 21:41:58 -0800

    Created Avatar pages

---
+++ client/src/tests/AvatarPage.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
+
+test('renders learn react link', () => {
+  render(<MemoryRouter>
+    <App />
+    </MemoryRouter>);
+});
