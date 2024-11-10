426f6b726d599044842c3735a8ffe9d920edfab0
Clarence Bermudez <csbermudez04@gmail.com>
2024-11-09 23:43:07 -0800

    Started adding UI elements

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import TimerPage from './pages/TimerPage';
+import AvatarPage from './pages/AvatarPage';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/timer/" element={<TimerPage />} />
         <Route path="/home/" />
+        <Route path="/avatar" element={<AvatarPage />} />
       </Routes>
     </div>
   );
--- client/src/components/UserAvatar.tsx
+++ client/src/components/UserAvatar.tsx
@@ -1,1 +1,12 @@
-export{}
+import '../styles/AvatarPageStyles.css';
+
+function UserAvatar(): JSX.Element {
+
+return (
+    <div className='userAvatar'>
+        <i className="bi bi-person-circle"></i>
+    </div>
+  );
+}
+
+export default UserAvatar;
