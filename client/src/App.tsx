1c503f23fc0b26d4a53589cf676a56e7cbd8a6f2
Clarence Bermudez <122479143+ClaraBlob@users.noreply.github.com>
2024-11-10 20:05:48 -0800

    Landing page (#7)
    * Created LandingPage branch
    
    * Created Landing Page
    
    * Moved the styles into a css file
    
    * added login button component and Landing page tests
    
    * added tests
    
    * added test cases

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import CalendarPage from "./pages/CalendarPage";
 import ProfilePage from "./pages/ProfilePage";
 import ChecklistPage from './pages/Checklist';
+import LandingPage from "./pages/LandingPage";
+import TimerPage from './pages/TimerPage';
+<<<<<<< HEAD
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import TimerPage from './pages/TimerPage';
+=======
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import CalendarPage from "./pages/CalendarPage";
+import ProfilePage from "./pages/ProfilePage";
+import ChecklistPage from './pages/Checklist';
+>>>>>>> 18979bcb1aefc643b6b06257bbe7dfbad784e686
 
 const App = () => {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/landing" element={<LandingPage />} />
         <Route path="/login/" element={<LoginPage />} />
         <Route path="/calendar/" element={<CalendarPage />} />
         <Route path="/profile/" element={<ProfilePage />} />
@@ -20,4 +33,8 @@ const App = () => {
   );
  };
 
+<<<<<<< HEAD
  export default App;
+=======
+ export default App;
+>>>>>>> 18979bcb1aefc643b6b06257bbe7dfbad784e686
--- client/src/components/LoginButton.tsx
+++ client/src/components/LoginButton.tsx
@@ -1,1 +1,21 @@
-export {}
+import Button from 'react-bootstrap/Button';
+import '../styles/LandingPageStyles.css';
+import { useNavigate } from "react-router-dom";
+
+function LoginButton(): JSX.Element {
+  let navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    let path = `/login`;
+    navigate(path);
+  }
+
+return (
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',}}>
+    <Button className='loginButton' role="button" onClick={routeChange} variant="primary">
+      Login
+    </Button>
+  </div>
+  );
+}
+
+export default LoginButton;
