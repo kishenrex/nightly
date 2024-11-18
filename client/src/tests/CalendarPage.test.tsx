a6089f3b9674bebea73050c0bf7b0d846572021a
doriszh0 <doodledodo8@gmail.com>
2024-11-17 21:25:16 -0800

    finished majority of functionality for calendar and checklist

--- client/src/tests/CalendarPage.test.tsx
+++ client/src/tests/CalendarPage.test.tsx
@@ -1,2 +1,9 @@
-// placeholder
-export {}
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import ChecklistPage from '../pages/Checklist';
+
+test('Add night routine', () => {
+  render(<ChecklistPage />);
+  const addRoutineButton = screen.getByText("Add New Routine");
+  expect(addRoutineButton).toBeInTheDocument();
+});
